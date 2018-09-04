a9f186647f222c3c30e805ec7705ec525cb768ea
mdandyf <mochamad.dandy@gmail.com>
2018-09-04 23:36:58 +0200

    adding splash before start main activity

--- SplashActivity.java
+++ SplashActivity.java
@@ -1,13 +1,32 @@
 package com.gojek.exercise.weather;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 1000;
+    Animation rotation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        rotation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        findViewById(R.id.imageView).startAnimation(rotation);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
     }
 }
+
